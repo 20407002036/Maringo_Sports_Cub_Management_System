a3428c46cc4c9c9e35520362c30455fb3773441b
Solomon <kyobenimaru001@gmail.com>
2023-10-03 17:31:59 +0300

    commit for the files immidiately after getting them from Comp Lab 1

---
+++ MaringoSportsClubSystem/src/maringosportsclubsystem/MaringoSportsClubSystem.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package maringosportsclubsystem;
+
+/**
+ *
+ * @author COMP LAB 1
+ */
+public class MaringoSportsClubSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Loginform log = new Loginform();
+        log.show();
+    }
+    
+}
